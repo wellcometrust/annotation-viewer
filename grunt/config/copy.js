eedaf79fe13c79126d51c61d0865f64825083981
Stephen Fraser <stephen.fraser@digirati.co.uk>
2016-11-28 10:52:10 +0000

    Foundation update to scripts to remove manual step from deployments

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -2,16 +2,29 @@
 
 module.exports = {
   dev: {
-    expand: true, 
-    cwd: 'src/asset-files', 
-    src: ['**'], 
+    expand: true,
+    cwd: 'src/asset-files',
+    src: ['**'],
     dest: 'build/assets'
   },
 
   dist: {
-    expand: true, 
-    cwd: 'src/asset-files', 
-    src: ['**'], 
+    expand: true,
+    cwd: 'src/asset-files',
+    src: ['**'],
     dest: 'dist/assets'
+  },
+
+  quilt_file: {
+    expand: true,
+    cwd: 'dist',
+    src: ['quilt.html'],
+    dest: 'quilt/index.html',
+  },
+  quilt_assets: {
+    expand: true,
+    cwd: 'dist/assets',
+    src: ['**'],
+    dest: 'dist/quilt/assets',
   },
 };
--- grunt/tasks/dist.js
+++ grunt/tasks/dist.js
@@ -1,5 +1,5 @@
 // grunt/tasks/dist.js
 
 module.exports = function(grunt) {
-  grunt.registerTask('dist', ['assemble:dist', 'scsslint', 'sass:dist','postcss:dist', 'browserify:dist', 'accessibility', 'groc', 'modernizr:dist', 'copy:dist', 'browserSync:dist']);
+  grunt.registerTask('dist', ['assemble:dist', 'scsslint', 'sass:dist','postcss:dist', 'browserify:dist', /*'accessibility',*/ 'groc', 'modernizr:dist', 'copy:dist', 'copy:quilt_file', 'copy:quilt_assets', 'browserSync:dist']);
 };
